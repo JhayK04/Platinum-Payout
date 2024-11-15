3655ae298b7f0c3974c92a7434c4f6a5caa0567e
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-11-15 02:42:43 +0100

    configured-gradient-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: {
+        'gradient': 'linear-gradient(to right, #FEC636, #DE680A)',
+      },
+    },
   },
   plugins: [],
 }
